214f28a17080bada1388c1d03e128edc3097b9ed
thiagofcm <thiagof1503@gmail.com>
2022-02-01 19:43:20 -0300

    commit 4

--- ExercicioEmpregadoCLT-HORISTA/Program.cs
+++ ExercicioEmpregadoCLT-HORISTA/Program.cs
@@ -6,7 +6,7 @@ namespace ExercicioEmpregadoCLT_HORISTA
     {
         static void Main(string[] args) {
             EmpregadoCLT F1 = new EmpregadoCLT("Thiago", "Cuevas", "12593512628", 5000.0);
-            EmpregadoHORISTA F2 = new EmpregadoHORISTA("Ana Clara", "Souza", "16828333802", 60, 10);
+            EmpregadoHORISTA F2 = new EmpregadoHORISTA("Ana Clara", "Vieira", "16828333802", 60, 10);
             Console.WriteLine("O salario do funcionario " + F1.getprimeironome() + "eh: " + F1.getSalario());
             F1.getsalarioliquido();
             F2.getsalarioliquido();
